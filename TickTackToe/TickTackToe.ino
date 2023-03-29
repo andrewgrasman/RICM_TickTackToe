a0c46a57e519a9a850ce8095200caaa513faa20c
--get <andrew.grasman@gmail.com>
2023-03-29 15:26:05 -0400

    3/29/23 work

--- TickTackToe/TickTackToe.ino
+++ TickTackToe/TickTackToe.ino
@@ -36,7 +36,7 @@
 // On Mega/ADK/Due, SDA == Digital 20, SCL == Digital 21
 
 // Use I2C, no reset pin!
-Adafruit_CAP1188 cap = Adafruit_CAP1188();
+//Adafruit_CAP1188 cap = Adafruit_CAP1188();
 
 // Or...Use I2C, with reset pin
 //Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_RESET);
@@ -46,41 +46,51 @@ Adafruit_CAP1188 cap = Adafruit_CAP1188();
 
 // Or.. Software SPI: clock, miso, mosi, cs, reset
 //Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_CLK, CAP1188_MISO, CAP1188_MOSI, CAP1188_CS, CAP1188_RESET);
-
+enum led {off,red,green,both};
+enum led ledArr[9];
+enum touchpad {CP8,CP7,CP6,CP5,CP4,CP3,CP2,CP1,CP16,CP15,CP14,CP13,
+               CP12,CP11,CP10,CP9,CP24,CP23,CP22,CP21,CP20,CP19,CP18};
+enum touchpad touchpads[24];
+Adafruit_CAP1188 cap[4];
 void setup() {
   Serial.begin(9600);
   Serial.println("CAP1188 test!");
-
   // Initialize the sensor, if using i2c you can pass in the i2c address
-  if (!cap.begin(0x2A)) {
+  for(int i=0;i<sizeof(cap);i++){
+    cap[i]=Adafruit_CAP1188();
+  }
+  if (!cap[1].begin(0x2A)) {
     Serial.println("CAP1188 at 0x2A not found");
   }
   else {
        Serial.println("CAP1188 at 0x2A found!");
   }
-  if (!cap.begin(0x2B)) {
+  if (!cap[2].begin(0x2B)) {
     Serial.println("CAP1188 at 0x2B not found");
   }
   else {
        Serial.println("CAP1188 at 0x2B found!");
   }
-  if (!cap.begin(0x2C)) {
+  if (!cap[3].begin(0x2C)) {
     Serial.println("CAP1188 at 0x2C not found");
   }
   else {
        Serial.println("CAP1188 at 0x2C found!");
   }
-  if (!cap.begin(0x29)) {
+  if (!cap[0].begin(0x29)) {
     Serial.println("CAP1188 at 0x29 not found");
   }
   else {
        Serial.println("CAP1188 at 0x29 found!");
-       cap.LED
+       //cap.LED
   }
+  
+  for(int i=1;i<sizeof(cap);i++)cap[i].writeRegister(CAP1188_LEDLINK,0);//turn of the led linking with the touchpads
+  initLeds();
 }
 
 void loop() {
-  uint8_t touched = cap.touched();
+  uint8_t touched = cap[0].touched();
   if (touched == 0) {
     // No touch detected
     return;
@@ -92,4 +102,10 @@ void loop() {
   }
   Serial.println();
   delay(50);
+}
+
+void initLeds(){
+  for(int i=0; i<sizeof(ledArr); i++){
+    ledArr[i]=off;
+  }
 }
