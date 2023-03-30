89a4e53f47485d9853ba666d85a90b49222a06a4
--get <andrew.grasman@gmail.com>
2023-03-30 02:40:29 -0400

    I think tick tack toe and a reset pad

--- TickTackToe/TickTackToe.ino
+++ TickTackToe/TickTackToe.ino
@@ -1,29 +1,30 @@
 /*************************************************** 
-  This is a library for the CAP1188 I2C/SPI 8-chan Capacitive Sensor
+	This is a library for the CAP1188 I2C/SPI 8-chan Capacitive Sensor
 
-  Designed specifically to work with the CAP1188 sensor from Adafruit
-  ----> https://www.adafruit.com/products/1602
+	Designed specifically to work with the CAP1188 sensor from Adafruit
+	----> https://www.adafruit.com/products/1602
 
-  These sensors use I2C/SPI to communicate, 2+ pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
+	These sensors use I2C/SPI to communicate, 2+ pins are required to  
+	interface
+	Adafruit invests time and resources providing this open source code, 
+	please support Adafruit and open-source hardware by purchasing 
+	products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
+	Written by Limor Fried/Ladyada for Adafruit Industries.  
+	BSD license, all text above must be included in any redistribution
  ****************************************************/
  
 #include <Wire.h>
 //#include <SPI.h>
 #include <Adafruit_CAP1188.h>
 
+/*
+TODO: Check if code is needed needed or change to pi
+// Reset pin could be worth adding in hardware, definitions unused
 // Reset Pin is used for I2C or SPI
 // These are defined for software SPI, for hardware SPI, check your 
-// CS pin is used for software or hardware SPI
+// CS pin is used for software or hardware SPI 
 #define CAP1188_RESET  9
-
-
 #define CAP1188_CS  10
 // board's SPI pins in the Arduino documentation
 #define CAP1188_MOSI  11
@@ -46,66 +47,90 @@
 
 // Or.. Software SPI: clock, miso, mosi, cs, reset
 //Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_CLK, CAP1188_MISO, CAP1188_MOSI, CAP1188_CS, CAP1188_RESET);
-enum led {off,red,green,both};
-enum led ledArr[9];
-enum touchpad {CP8,CP7,CP6,CP5,CP4,CP3,CP2,CP1,CP16,CP15,CP14,CP13,
-               CP12,CP11,CP10,CP9,CP24,CP23,CP22,CP21,CP20,CP19,CP18};
-enum touchpad touchpads[24];
-Adafruit_CAP1188 cap[4];
-void setup() {
-  Serial.begin(9600);
-  Serial.println("CAP1188 test!");
-  // Initialize the sensor, if using i2c you can pass in the i2c address
-  for(int i=0;i<sizeof(cap);i++){
-    cap[i]=Adafruit_CAP1188();
-  }
-  if (!cap[1].begin(0x2A)) {
-    Serial.println("CAP1188 at 0x2A not found");
-  }
-  else {
-       Serial.println("CAP1188 at 0x2A found!");
-  }
-  if (!cap[2].begin(0x2B)) {
-    Serial.println("CAP1188 at 0x2B not found");
-  }
-  else {
-       Serial.println("CAP1188 at 0x2B found!");
-  }
-  if (!cap[3].begin(0x2C)) {
-    Serial.println("CAP1188 at 0x2C not found");
-  }
-  else {
-       Serial.println("CAP1188 at 0x2C found!");
-  }
-  if (!cap[0].begin(0x29)) {
-    Serial.println("CAP1188 at 0x29 not found");
-  }
-  else {
-       Serial.println("CAP1188 at 0x29 found!");
-       //cap.LED
-  }
-  
-  for(int i=1;i<sizeof(cap);i++)cap[i].writeRegister(CAP1188_LEDLINK,0);//turn of the led linking with the touchpads
-  initLeds();
+*/
+
+enum led{off,red,green,both}ledArr[]={0,0,0,0,0,0,0,0,0};
+int board[][]={
+	{1,2,3,4,5,6,7,8,9},
+	{10,11,12,13,14,15,16,17,18},
+	{19,20,21,22,23,24}
+	};
+bool oldTouchState[3][];
+enum player{green,red}
+#define START_PLAYER 0;
+enum player curPlayer=START_PLAYER;
+uint8_t CAP_I2C_ADDR[]={0x29,0x2A,0x2B,0x2C};
+Adafruit_CAP1188 cap[sizeof(CAP_I2C_ADDR)];
+
+void setup(){
+	Serial.begin(9600);
+	Serial.println("CAP1188 test!");
+	// Initialize the sensor, if using i2c you can pass in the i2c address
+	for(int i=0;i<sizeof(cap);i++){
+		cap[i]=Adafruit_CAP1188(CAP_I2C_ADDR[i]);
+	}
+	for(int i=0;i<sizeof(cap);i++){
+		if(!cap[i].begin())
+		Serial.println("CAP1188 at "+CAP_I2C_ADDR[i]+" not found");
+			else Serial.println("CAP1188 at 0"+CAP_I2C_ADDR[i]+"x2A found!");
+		cap[i].writeRegister(CAP1188_LEDLINK,0x00);//turn of the led linking with the touchpads
+		}
 }
 
-void loop() {
-  uint8_t touched = cap[0].touched();
-  if (touched == 0) {
-    // No touch detected
-    return;
-  }  
-  for (uint8_t i=0; i<8; i++) {
-    if (touched & (1 << i)) {
-      Serial.print("CP"); Serial.print(8-i); Serial.print("\t");
-    }
-  }
-  Serial.println();
-  delay(50);
+void loop(){
+	touchHandler(checkToutches());
+	curPlayer=(curPlayer+1)%2;//switch board[0]current player
+	updateLeds();
+	delay(50);
 }
 
-void initLeds(){
-  for(int i=0; i<sizeof(ledArr); i++){
-    ledArr[i]=off;
-  }
+void touchHandler(bool state[24]){
+	for(int b=0;b<sizeof(board);b++)
+		for(int c=0;)c<sizeof(board[b];c++){
+			if(state[b*sizeof(board[0])+c] && !oldTouchState[b][c])
+			if(b<2)play(b,c);
+			else utility(c);
+		}
+}
+
+bool* checkToutches(){
+	bool result[8(sizeof(cap)-1)];
+	for(int a=0;a<sizeof(cap)-1;a++)//fourth cap1188 has no touchpads
+	for(int b=0;b<8;b++)//number of toutchpads
+	result[a][b]=cap[a];
+	return result;
+}
+
+void play(int player, int button){
+	if(ledArr[button]==0 && player==curPlayer)
+	ledArr[button]=player+1;
+}
+
+void utility(int button){
+	if(button==0)
+	reset()
+	if(button>0)
+	Serial.println("NOT IMPLEMENTED BUTTON IN UTILITY")
+}
+
+void updateLeds(){
+	uint8_t result;
+	for(int color=0;color<2;color++)
+	for(int i=0;i<sizeof(ledArr);i++)
+	if(i<8){//8 leds per cap1188
+		result*=2;
+		result+ledArr[i]
+	} else
+	cap[i/8].writeRegister(0x74,result)//only uses the first two cap1188s
+}
+
+reset(){
+	clearLeds();
+	curPlayer=green;
+}
+
+void clearLeds(){
+	for(int i=0; i<sizeof(ledArr); i++){
+		ledArr[i]=off;
+	}
 }
